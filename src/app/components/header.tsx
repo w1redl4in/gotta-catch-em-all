52b8c8b974fa6d58acdb5ea7770721dc9511f3fc
Felipe <felipe.austriaco@mercadolivre.com>
2024-02-07 07:43:48 -0300

    fix(components): :pencil2: removing e accent from header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,7 +4,7 @@
 export function Header() {
   return (
     <div className="font-display shadow-md h-[45vh] bg-background2 bg-no-repeat bg-cover bg-bottom flex flex-col items-center justify-center">
-      <h1 className="text-shadow-xl font-medium text-8xl text-zinc-100">Pokédex</h1>
+      <h1 className="text-shadow-xl font-medium text-8xl text-zinc-100">Pokedex</h1>
       <h2 className="text-shadow-xl font-medium text-4xl text-zinc-100">
         Gotta Catch 'Em All
       </h2>
