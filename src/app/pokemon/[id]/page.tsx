b0b3187d350246eff07f3aab8e14c8a0cb126084
Felipe <felipe.austriaco@mercadolivre.com>
2024-02-07 12:51:28 -0300

    style(pokemon): :lipstick: some style changes

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -32,7 +32,7 @@ const fromColors = {
   purple: 'from-purple-400/60',
   yellow: 'from-yellow-400/60',
   brown: 'from-amber-700/60',
-  white: 'from-zinc-300/60',
+  white: 'from-zinc-800/60',
 }
 
 const bgColors = {
@@ -42,7 +42,7 @@ const bgColors = {
   purple: 'bg-purple-500/45',
   yellow: 'bg-yellow-500/45',
   brown: 'bg-amber-700/45',
-  white: 'bg-zinc-300/45',
+  white: 'bg-zinc-500/45',
 }
 
 const progressColors = {
@@ -62,7 +62,7 @@ export default async function Pokemon({ params }: PokemonProps) {
 
   const { pokemon, species } = await getPokemon(params.id)
 
-  const description = species.flavor_text_entries[0].flavor_text
+  const description = species.flavor_text_entries[1].flavor_text
   const fromColor = fromColors[species.color.name as keyof typeof fromColors]
   const bgColor = bgColors[species.color.name as keyof typeof bgColors]
   const progressColor = progressColors[species.color.name as keyof typeof progressColors]
@@ -71,11 +71,11 @@ export default async function Pokemon({ params }: PokemonProps) {
     <div className={`flex justify-around min-h-screen w-full bg-gradient-to-b ${fromColor} to-zinc-300`}>
       <div className="flex justify-around items-center w-[80%]">
         <article className={`flex flex-col max-w-[300px] rounded-md shadow-lg ${bgColor} ring-1 ring-black/5 text-white`}>
-          <div className="text-center p-3 text-2xl">
+          <div className="text-center p-3 text-4xl">
             <h1 className="text-shadow-xl">{pokemon.name.toLocaleUpperCase()}</h1>
           </div>
           <div className="p-6 leading-loose">
-            <h1 className="text-shadow-xl text-lg">{description}</h1>
+            <h1 className="text-shadow-xl text-3xl">{description}</h1>
           </div>
         </article>
         <Image
@@ -83,16 +83,15 @@ export default async function Pokemon({ params }: PokemonProps) {
           height={200}
           width={200}
           alt={pokemon.name}
-          src={pokemon.sprites.other["official-artwork"].front_default}
-        // src={pokemon.sprites.other.dream_world.front_default}
+          src={pokemon.sprites.other.dream_world.front_default}
         />
         <article className={`flex flex-col min-w-64 rounded-md shadow-lg ${bgColor} ring-1 ring-black/5`}>
           <div className="flex flex-col text-white p-6 gap-10">
-            <h2 className="text-center font-semibold text-shadow-lg text-lg">
+            <h2 className="text-center font-semibold text-shadow-lg text-4xl">
               {pokemon.name.toLocaleUpperCase()} STATUS
             </h2>
             {pokemon.stats.map(stat => (
-              <div key={stat.stat.name} className="flex flex-col gap-2">
+              <div key={stat.stat.name} className="flex flex-col gap-2 text-2xl">
                 <span className="flex gap-1">
                   {stat.stat.name}
                   <span>
